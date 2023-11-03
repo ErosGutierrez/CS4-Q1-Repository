8e2685cb35864d3dad8335aefd49836daa099226
ErosGutierrez <114297569+ErosGutierrez@users.noreply.github.com>
2023-11-03 19:35:51 +0800

    Origin

---
+++ src/ele/pkg13_repository/Q1Repos_ele_gutierrez_gl.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ele.pkg13_repository;
+
+/**
+ *
+ * @author Gabriel Gutierrez
+ */
+public class Q1Repos_ele_gutierrez_gl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("CS4 Q1 Activity Repository");
+      
+    }
+    
+}
