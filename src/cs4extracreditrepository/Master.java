3f344889d392a76764061d5f44202fefbe5b1d8d
ErosGutierrez <114297569+ErosGutierrez@users.noreply.github.com>
2023-11-03 18:54:52 +0800

    Master File

---
+++ src/cs4extracreditrepository/Master.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cs4extracreditrepository;
+
+/**
+ *
+ * @author Gabriel Gutierrez
+ */
+public class Master {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Master File");
+        System.out.println("1. Act1");
+        System.out.println("2. Act2");
+        System.out.println("3. Act3");
+        System.out.println("4. Act4");
+        System.out.println("5. Act 5");
+    }
+    
+}
